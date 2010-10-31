1be69ffcbf9c30cb965246adadd40afa099c663b
Tim Butram <atrus6@gmail.com>
2010-10-31 17:43:54 -0400

    Main WorldsProject site now works. Next up will be making the Fluxware site using the templating system.

--- project.php
+++ project.php
@@ -7,7 +7,7 @@ $page->replace_tags(array(
   "title" => "Our Projects", 
   "heading" => "Current (and some past) Projects", 
   "content" => '<ul class="img_green">
-                        <li><h3><a href="/fluxware/index.html">Fluxware</a></h3></li>
+                        <li><h3><a href="/fluxware/index.php">Fluxware</a></h3></li>
                         <p>Fluxware is a Game Engine and Game Development Toolkit, that facilitates the easier creation
                         of new games. Currently the Engine is only available in the Java programming language, however there
                         are plans to migrate to other languages as well, including C#, Python and C++.
