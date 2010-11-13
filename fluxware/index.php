f3be70e7c7d940556a7ec16cc6c4f8342a9a5da0
Tim Butram <atrus6@gmail.com>
2010-11-13 12:15:38 -0500

    Fixed spelling error on the main page.

--- fluxware/index.php
+++ fluxware/index.php
@@ -10,7 +10,7 @@ $page->replace_tags(array(
           rite Editor and Network connectivity software. Our goal is to let anyone have the ability to
           create free and <i>fun</i> games without the need for massive amounts of programming and artistic skill. 
           <br />
-          Fluxware focuses on using free, open source technolgy, which enambles you to make any game you want,  your way, with no restrictions. Have fun, be creative, and make a game that you and your friends can all play.'
+          Fluxware focuses on using free, open source technolgy, which enables you to make any game you want,  your way, with no restrictions. Have fun, be creative, and make a game that you and your friends can all play.'
 )); 
 
 $page->output();
